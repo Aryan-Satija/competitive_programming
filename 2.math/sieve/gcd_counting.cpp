a77dcdd4cefa74c53f71e9a1c08a9bac9aaf9ca3
aryan <aryansatija2003@gmail.com>
2024-02-08 14:36:52 +0530

    +1

---
+++ 2.math/sieve/gcd_counting.cpp
@@ -0,0 +1,30 @@
+// 2000
+// CODEFORCES
+// https://codeforces.com/contest/1101/problem/D
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MOD 1000000007
+
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vb vector<bool>
+#define vvi vector<vector<int>>
+#define vvll vector<vector<long long int>>
+#define vc vector<char>
+#define vll vector<long long>
+#define mii map<int, int>
+#define umii unordered_map<int, int>
+#define si set<int>
+#define rep(i, n) for(ll i = 0; i < n; i++)
+#define ren(i, n) for(ll i = (n-1); i >= 0; i--)
+
+int main(){
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL);   
+    cout.tie(NULL);
+    
+}
