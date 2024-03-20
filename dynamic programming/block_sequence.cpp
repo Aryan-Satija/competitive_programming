d83a4921c91d5a13c8deacac8e89423372e145c9
aryan <aryansatija2003@gmail.com>
2024-03-20 18:18:19 +0530

    +1

---
+++ dynamic programming/block_sequence.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define mod 1000000007
+ 
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vb vector<bool>
+#define vvi vector<vector<int>>
+#define vvb vector<vector<bool>>
+#define vvc vector<vector<char>>
+#define vvll vector<vector<long long int>>
+#define vc vector<char>
+#define vll vector<long long>
+#define mii map<int, int>
+#define umii unordered_map<int, int>
+#define si set<int>
+#define rep(i, s, e) for(ll i = s; i <= e; i++)
+#define ren(i, s, e) for(ll i = s; i >= e; i--)
+ 
+void solve(){
+    //* Write code here
+    int n;
+    cin >> n;
+    vi arr(n);
+
+    rep(i, 0, n-1) cin >> arr[i];
+
+    vi dp(n+1, 0);
+
+    dp[n-1] = 1; // the only way to make the subarray[n-1 ... n-1] beautiful is by deleting that element
+
+    ren(i, n-2, 0){
+        dp[i] = 1 + dp[i+1];
+        if(i + arr[i] + 1 <= n) dp[i] = min(dp[i], dp[i + arr[i] + 1]); 
+    }
+
+    cout << dp[0] << endl;
+}
+int main(){
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL);   
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--){
+        solve(); 
+    }
+} 
