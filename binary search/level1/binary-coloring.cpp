523aa951fa40d8a3f9743d4979ced76c362343c4
aryan <aryansatija2003@gmail.com>
2024-06-07 12:01:51 +0530

    +1

---
+++ binary search/level1/binary-coloring.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mod 1000000007
+
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vb vector<bool>
+#define vvi vector<vector<int>>
+#define vvb vector<vector<bool>>
+#define vvc vector<vector<char>>
+#define vvll vector<vector<long long int>>
+#define vc vector<char>
+#define vll vector<long long>
+#define mii map<int, int>
+#define umii unordered_map<int, int>
+#define si set<int>
+#define rep(i, s, e) for (ll i = s; i <= e; i++)
+#define ren(i, s, e) for (ll i = s; i >= e; i--)
+
+void solve(){
+    //* write your code here
+    ll n;
+    cin >> n;
+    vi bits(50, 0);
+    int id = 0;
+    while(n > 0){
+        bits[id] = (n&1);
+        n = n/2;
+        id++;
+    }
+    int len = 0;
+    id = 0;
+    rep(id, 1, 32){
+        if(bits[id] == 1){
+            if(bits[id-1] == 1){
+                bits[id] = 0;
+                bits[id+1]++;
+                bits[id-1] = -1;
+            }
+        }
+        else if(bits[id] == 2){
+            bits[id] = 0;
+            bits[id+1]++;
+        }
+        len++;
+    }
+    len = 32;
+    while(bits[len] == 0) len--;
+    cout << len+1 << endl;
+    rep(i, 0, len) cout << bits[i] << " ";
+    cout << endl; 
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
